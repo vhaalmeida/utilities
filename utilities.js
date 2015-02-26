7ddbe530c28635a93b2eee1956dbbd2ffde91231
VH <VH@Air-de-VH.Home>
2015-02-26 15:20:49 -0300

    Victor
    Added new function: getItemByPropertyValue
    Removed functions: vtex

--- utilities.js
+++ utilities.js
@@ -34,40 +34,11 @@ var utilities = {
 	//add classes to html tag to crossbrowser
 	htmlClasses : function(){
 		var e="unknownBrowser";if($.browser.msie){e="msie msie"+parseInt($.browser.version,10)}else if($.browser.webkit){var t=navigator.userAgent.toLowerCase();if(t.indexOf("chrome")===-1){e="safari"}else{e="chrome"}}else if($.browser.mozilla){e="mozilla"}else if($.browser.opera){e="opera"}else if($.browser.camino){e="camino"}$("html").addClass(e);var n="unknownOs";if(navigator.appVersion.indexOf("Win")!=-1){n="windows"}else if(navigator.appVersion.indexOf("Mac")!=-1){n="mac"}else if(navigator.appVersion.indexOf("X11")!=-1){n="unix"}else if(navigator.appVersion.indexOf("Linux")!=-1){n="linux"}$("html").addClass(n);
-	}
-	,
-	vtex: {
-		//Coloca a Ref do nome do produto entre span
-		splitProductRef: function(){
-			if($('.productName').length > 0){
-				var nome = $('.productName').html().split('Ref.');
-				$('.productName').html('');
-				if(nome[1] == undefined){
-					$('.productName').append('<span class="name">'+nome[0]+'</span><span class="ref">Ref. '+$('.value-field.Referencia-Cor').html()+'</span>');
-				}else{
-					$('.productName').append('<span class="name">'+nome[0]+'</span><span class="ref">Ref. '+nome[1]+'</span>');
-				}
-				}
-			
-		}
-		,
-		//Coloca a Ref do nome de cada produto de uma prateleira emtre span
-		splitPrateleiraRef: function(){
-			if($('.prateleira li[layout]:not(".ajax-formated")').length > 0){
-				$('.prateleira li[layout]:not(".ajax-formated")').each(function(){
-					if($(this).find('h3 a').length){
-						var productName = $(this).find('h3 a').html();
-						if(productName.indexOf('Ref.') != -1)
-						{
-							var productNameSplited = productName.split('Ref.');
-							$(this).find('h3 a').html('<span class="name">'+productNameSplited[0]+'</span><span class="ref">Ref. '+productNameSplited[1]+'</span>');
-						}
-						$(this).addClass('ajax-formated');
-					}
-					
-			   });
-			}
-			
-		}
+	},
+	//get item of an array or object by property value
+	getItemByPropertyValue : function(object,property,value){
+		return object.filter(function(obj) {
+			return obj[property] == value;
+		});
 	}
 }
